39b0ad4e68a0ad6708f64f949c7a70c112af9f97
James <jatkinson15@pm.me>
2025-10-17 07:11:59 +0100

    Update CavePreMiner to adjust height threshold and add PlayerDeathItems functionality with detailed item spawning configuration

---
+++ src/main/java/horror/blueice129/feature/PlayerDeathItems.java
@@ -0,0 +1,12 @@
+package horror.blueice129.feature;
+
+import net.minecraft.util.math.random.Random;
+
+/**
+ * handles having items spawn as if a player had died
+ */
+
+public class PlayerDeathItems {
+    private static final Random random = Random.create();
+    
+}
