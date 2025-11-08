54ce2d5bbfcc4fed03f7391f78f1b6527461a153
James <jatkinson15@pm.me>
2025-11-08 17:28:15 +0000

    plan out state-based behavior for Blueice129 entity

--- src/main/java/horror/blueice129/entity/Blueice129Entity.java
+++ src/main/java/horror/blueice129/entity/Blueice129Entity.java
@@ -9,7 +9,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 
 /**
- * Blueice129 Entity - A custom PathAwareEntity that takes the form of a player character
+ * Blueice129 Entity - A custom PathAwareEntity that takes the form of a player
+ * character
  * with Blueice129's skin.
  * 
  * PathAwareEntity extends MobEntity, which extends LivingEntity.
@@ -18,6 +19,95 @@ import net.minecraft.world.World;
  * - PathAwareEntity has pathfinding favor and slightly tweaked leash behavior.
  */
 public class Blueice129Entity extends PathAwareEntity {
+
+    // TODO: Implement state-based behavior using currentState
+    // TODO: Implement PASSIVE goal
+    // TODO: Implement PANICED goal
+    // TODO: Implement SURFACE_HIDING goal
+    // TODO: Implement UNDERGROUND_BURROWING goal
+    // TODO: Implement IN_MENUS goal
+    // TODO: Implement INVESTIGATING goal
+    // TODO: Implement UPGRADING_HOUSE goal
+
+    private EntityState currentState = EntityState.PASSIVE;
+    private final int costlyCheckTickCooldown = 10;
+    private int costlyCheckTickCounter = 0;
+
+    public enum EntityState {
+        PASSIVE, // Default state, does nothing really
+        PANICED, // jumps away from player, cycles hotbar, head jerks around like mouse is being
+                 // shaken, crouches randomly
+        SURFACE_HIDING, // hides behind trees/other structures, peeks out occasionally, always crouched
+        UNDERGROUND_BURROWING, // crouched in the ground near player, mines blocks when player does, attempts
+                               // to follow player
+        IN_MENUS, // stops moving, no actions taken.
+        INVESTIGATING, // opens chests, furnaces, opens doors, presses buttons and levers, generally
+                       // interacts with the environment
+        UPGRADING_HOUSE // places blocks, breaks old blocks, clears out area, uses blocks and tools from
+                        // inventory
+    }
+
+    /**
+     * Change the entity's state and update AI goals accordingly
+     */
+    public void setState(EntityState newState) {
+        if (this.currentState == newState)
+            return;
+
+        this.currentState = newState;
+        updateGoals();
+    }
+
+    /**
+     * Update AI goals based on the current state.
+     * This method clears existing goals and adds new ones based on state.
+     */
+    private void updateGoals() {
+        // TODO: Clear existing goals
+        // TODO: finish this method
+        // this.goalSelector.remove(null);
+    }
+
+    /**
+     * will handle automatic state transitions and per-tick behavior
+     */
+    @Override
+    public void tick() {
+        super.tick();
+        costlyCheckTickCounter++;
+        boolean shouldPerformCostlyCheck = costlyCheckTickCounter >= costlyCheckTickCooldown;
+
+        switch (currentState) {
+            case PASSIVE:
+                // TODO: Implement transitions from PASSIVE to other states
+                break;
+            case PANICED:
+                // TODO: Implement transitions from PANICED to other states
+                break;
+            case SURFACE_HIDING:
+                // TODO: Implement transitions from SURFACE_HIDING to other states
+                break;
+            case UNDERGROUND_BURROWING:
+                // TODO: Implement transitions from UNDERGROUND_BURROWING to other states
+                break;
+            case IN_MENUS:
+                // TODO: Implement transitions from IN_MENUS to other states
+                break;
+            case INVESTIGATING:
+                // TODO: Implement transitions from INVESTIGATING to other states
+                break;
+            case UPGRADING_HOUSE:
+                // TODO: Implement transitions from UPGRADING_HOUSE to other states
+                break;
+        }
+    }
+
+    /**
+     * Get the current state of the entity
+     */
+    public EntityState getState() {
+        return currentState;
+    }
 
     public Blueice129Entity(EntityType<? extends PathAwareEntity> entityType, World world) {
         super(entityType, world);
@@ -29,17 +119,12 @@ public class Blueice129Entity extends PathAwareEntity {
      */
     @Override
     protected void initGoals() {
-        // Priority 0: Swimming - highest priority to avoid drowning
+
+        // Basic goals applicable in all states, can be overridden in state-specific updates
+        // TODO: Add state-specific goals in updateGoals()
         this.goalSelector.add(0, new SwimGoal(this));
-        
-        // Priority 1: Look at player
         this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
-        
-        // Priority 2: Wander around
-        this.goalSelector.add(2, new WanderAroundFarGoal(this, 0.8D));
-        
-        // Priority 3: Look around randomly
-        this.goalSelector.add(3, new LookAroundGoal(this));
+        this.goalSelector.add(5, new LookAroundGoal(this));
     }
 
     /**
