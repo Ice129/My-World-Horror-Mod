a0b7ad73f47fa5ea864ab9b2bd99df88d0139338
Ice129 <91084504+Ice129@users.noreply.github.com>
2025-11-23 08:31:41 +0000

    Remove unused Vec3d import in Blueice129SpawnScheduler

--- src/main/java/horror/blueice129/scheduler/Blueice129SpawnScheduler.java
+++ src/main/java/horror/blueice129/scheduler/Blueice129SpawnScheduler.java
@@ -10,7 +10,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+// import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 
