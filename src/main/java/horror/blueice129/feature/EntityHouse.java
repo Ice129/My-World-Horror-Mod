b0e9688648eda883d0e506b43d4a6f2fb277049a
ice129 <jatkinson15@pm.me>
2026-02-13 14:07:37 +0000

    planning: add EntityHouse class with planned functions for house events

---
+++ src/main/java/horror/blueice129/feature/EntityHouse.java
@@ -0,0 +1,35 @@
+package horror.blueice129.feature;
+
+public class EntityHouse {
+
+// functions planned for this event
+// 
+// find house start location
+// - takes environment into account, like a flat ish forrest area, or plains
+// 
+// calculate house stage
+// - based off aggro
+//
+// place house blocks
+// - takes player location and chunk loading and player view into account
+//
+// update loot
+// - loot in chests and containers is updated
+// 
+// prepare environment
+// - clear trees and grass and level the area more
+//
+// calculate wood
+// - gets the nearest average tree type, and uses that type of wood for the house
+//
+// calculate player interaction
+// - if the player has taken items, broken blocks, or otherwise interacted with the house, log it, as it will be used later
+//
+// make path
+// - makes path from entity house to player house, in steps. kinda wandery but alays in the player bases direction
+//
+// is player in base
+// - checks if the player is in the base, will be used for events
+
+
+}
