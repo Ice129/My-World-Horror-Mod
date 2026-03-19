e231d940be9242ea8b03c10985d35c78300ae586
ice129 <jatkinson15@pm.me>
2026-03-19 02:35:24 +0000

    Add initial implementation of TwoPlayerSleep class with isEntitySleeping method

---
+++ src/main/java/horror/blueice129/feature/TwoPlayerSleep.java
@@ -0,0 +1,11 @@
+package horror.blueice129.feature;
+
+public class TwoPlayerSleep {
+
+    public static boolean isEntitySleeping() {
+        // check if its late night time
+        // check if the player is spam sleeping
+        // check if 
+    }
+
+}
