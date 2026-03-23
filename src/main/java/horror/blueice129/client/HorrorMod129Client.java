d00386f99bf3f8057c1d816cc7da334ae4cbc3d2
ice129 <jatkinson15@pm.me>
2026-03-23 06:13:56 +0000

    Add DisclaimerScheduler and DisclaimerScreen for user consent flow

--- src/main/java/horror/blueice129/client/HorrorMod129Client.java
+++ src/main/java/horror/blueice129/client/HorrorMod129Client.java
@@ -5,6 +5,7 @@ import horror.blueice129.HorrorMod129;
 import horror.blueice129.feature.VersionHud;
 import horror.blueice129.network.ClientPacketHandler;
 import horror.blueice129.scheduler.MinMusicSetter;
+import horror.blueice129.scheduler.DisclaimerScheduler;
 import horror.blueice129.utils.ScreenshotFromEntity;
 import horror.blueice129.client.entity.model.Blueice129EntityModel;
 import horror.blueice129.client.entity.renderer.Blueice129EntityRenderer;
@@ -26,6 +27,7 @@ public class HorrorMod129Client implements ClientModInitializer {
         
         // Register client-side features
         MinMusicSetter.initialize();
+        DisclaimerScheduler.initialize();
         VersionHud.initialize();
         ScreenshotFromEntity.initialize();
 
