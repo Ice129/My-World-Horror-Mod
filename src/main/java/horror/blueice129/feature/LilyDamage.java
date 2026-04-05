12f87519615ce0e64ac482dc3666246db2cc0bad
James <jatkinson15@pm.me>
2026-04-05 21:06:16 +0100

    implement lily sound management and networking for player interactions

--- src/main/java/horror/blueice129/feature/LilyDamage.java
+++ src/main/java/horror/blueice129/feature/LilyDamage.java
@@ -1,11 +1,15 @@
 package horror.blueice129.feature;
 
+import horror.blueice129.network.ModNetworking;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LilyDamage {
 
@@ -14,10 +18,14 @@ public class LilyDamage {
     private static final int DURABILITY_DRAIN_PER_FLOWER = 2;
 
     public static void applyToPlayer(ServerWorld world, ServerPlayerEntity player) {
-        int flowers = countNearbyFlowers(world, player.getBlockPos());
-        if (flowers == 0) return;
+        List<BlockPos> flowers = findNearbyFlowers(world, player.getBlockPos());
+        if (flowers.isEmpty()) return;
 
-        int drain = flowers * DURABILITY_DRAIN_PER_FLOWER;
+        for (BlockPos flowerPos : flowers) {
+            ModNetworking.sendLilyRainStart(player, flowerPos);
+        }
+
+        int drain = flowers.size() * DURABILITY_DRAIN_PER_FLOWER;
         PlayerInventory inventory = player.getInventory();
         ItemStack[] candidates = new ItemStack[inventory.size()];
         int candidateCount = 0;
@@ -35,17 +43,18 @@ public class LilyDamage {
         chosenStack.damage(drain, world.getRandom(), player);
     }
 
-    private static int countNearbyFlowers(ServerWorld world, BlockPos center) {
-        int count = 0;
+    private static List<BlockPos> findNearbyFlowers(ServerWorld world, BlockPos center) {
+        List<BlockPos> flowers = new ArrayList<>();
         for (int x = -H_RADIUS; x <= H_RADIUS; x++) {
             for (int z = -H_RADIUS; z <= H_RADIUS; z++) {
                 for (int y = -V_RADIUS; y <= V_RADIUS; y++) {
-                    if (world.getBlockState(center.add(x, y, z)).isOf(Blocks.LILY_OF_THE_VALLEY)) {
-                        count++;
+                    BlockPos flowerPos = center.add(x, y, z);
+                    if (world.getBlockState(flowerPos).isOf(Blocks.LILY_OF_THE_VALLEY)) {
+                        flowers.add(flowerPos.toImmutable());
                     }
                 }
             }
         }
-        return count;
+        return flowers;
     }
 }
--- src/main/java/horror/blueice129/network/ModNetworking.java
+++ src/main/java/horror/blueice129/network/ModNetworking.java
@@ -4,6 +4,9 @@ import horror.blueice129.HorrorMod129;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.Identifier;
 
 /**
@@ -12,6 +15,8 @@ import net.minecraft.util.Identifier;
 public class ModNetworking {
     public static final Identifier SETTINGS_TRIGGER_ID = new Identifier(HorrorMod129.MOD_ID, "settings_trigger");
     public static final Identifier ENTITY_SCREENSHOT_ID = new Identifier(HorrorMod129.MOD_ID, "entity_screenshot");
+    public static final Identifier LILY_RAIN_START_ID = new Identifier(HorrorMod129.MOD_ID, "lily_rain_start");
+    public static final Identifier LILY_RAIN_STOP_ID = new Identifier(HorrorMod129.MOD_ID, "lily_rain_stop");
 
     public static void registerPackets() {
         // No server-side registration required for simple S2C packets with Fabric.
@@ -36,5 +41,25 @@ public class ModNetworking {
         PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
         buf.writeInt(entityId);
         ServerPlayNetworking.send(player, ENTITY_SCREENSHOT_ID, buf);
+    }
+
+    public static void sendLilyRainStart(ServerPlayerEntity player, BlockPos pos) {
+        ServerPlayNetworking.send(player, LILY_RAIN_START_ID, writeBlockPos(pos));
+    }
+
+    public static void sendLilyRainStop(ServerPlayerEntity player, BlockPos pos) {
+        ServerPlayNetworking.send(player, LILY_RAIN_STOP_ID, writeBlockPos(pos));
+    }
+
+    public static void sendLilyRainStop(ServerWorld world, BlockPos pos) {
+        for (ServerPlayerEntity player : world.getPlayers(serverPlayer -> serverPlayer.squaredDistanceTo(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) <= 64.0)) {
+            sendLilyRainStop(player, pos);
+        }
+    }
+
+    private static PacketByteBuf writeBlockPos(BlockPos pos) {
+        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+        buf.writeBlockPos(pos);
+        return buf;
     }
 }
